4a36a5402d850c609a919764bdc34849fe3e759c
Rafael <rafaenazatj@gmail.com>
2022-05-12 15:32:47 -0300

    esse

---
+++ modelo.js
@@ -0,0 +1,18 @@
+
+var parcelas = Number ;
+var cliente = A;//precisa adicionar a variavel tabuada
+var valor = Number ;
+var cpf = Number;
+var usuario = Text ;
+var senha = Text;
+var produto = text ;
+
+
+
+
+
+console.log("Olá seja bem vindo: ")
+console.log("Para continuar, faça login")
+console.log("Digite seu nome de usuário: ")
+
+usuario();
